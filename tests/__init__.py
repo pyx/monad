0aa3ac9b054dd4b1f8d71213eb980418afe83e74
Philip Xu <pyx@xrefactor.com>
2014-03-21 01:03:41 -0400

    Added test directory
    --HG--
    extra : histedit_source : 8fb0e321a93f5f31824986d8a60d63fea4a9b04a

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012-2014, Philip Xu <pyx@xrefactor.com>
+# License: BSD New, see LICENSE for details.
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
