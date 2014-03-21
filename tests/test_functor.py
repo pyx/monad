8c4a7217020d99815f015872cf61122a638d8e78
Philip Xu <pyx@xrefactor.com>
2014-03-21 01:04:54 -0400

    Added tests for Functor

---
+++ tests/test_functor.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012-2014, Philip Xu <pyx@xrefactor.com>
+# License: BSD New, see LICENSE for details.
+import pytest
+
+from monad.types import Functor
+
+testee = [
+]
+test_range = range(-100, 100)
+ids = [t.__name__ for t in testee]
+
+
+def pytest_generate_tests(metafunc):
+    if 'functor' in metafunc.funcargnames:
+        metafunc.parametrize('functor', testee, ids=ids)
+
+
+def test_functor_is_abstract():
+    with pytest.raises(NotImplementedError):
+        Functor(1).fmap(lambda a: a)
+
+
+def test_fmap_functor_laws(functor):
+    identity = lambda a: a
+    f = lambda a: a + 1
+    g = lambda a: a * 2
+    f_g = lambda n: f(g(n))
+
+    for n in test_range:
+        ft = functor(n)
+        # fmap id == id
+        assert ft.fmap(identity) == identity(ft)
+        # fmap (f . g) == fmap f . fmap g
+        assert ft.fmap(f_g) == ft.fmap(g).fmap(f)
