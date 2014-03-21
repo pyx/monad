20e2156528616f946a27ed1608b6721eff4e4f43
Philip Xu <pyx@xrefactor.com>
2014-03-21 01:00:44 -0400

    Added package monad
    --HG--
    extra : histedit_source : fca8d13b454f392627fe3fabe502763998a01786

---
+++ monad/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012-2014, Philip Xu <pyx@xrefactor.com>
+# License: BSD New, see LICENSE for details.
+"""monad - a functional library"""
+
+__version__ = (0, 1)
+__release__ = '.dev'
+
+VERSION = '%d.%d' % __version__ + __release__
+
+__all__ = [
+]
