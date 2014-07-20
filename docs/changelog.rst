7b4daac18ba9d4f6b87a4472c72ef729a374035a
Philip Xu <pyx@xrefactor.com>
2014-07-20 16:07:39 -0400

    Bumped version number to 0.1

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -1,3 +1,7 @@
 =========
 Changelog
 =========
+
+- 0.1
+
+  First public release.
--- monad/__init__.py
+++ monad/__init__.py
@@ -4,7 +4,7 @@
 """monad - a functional library"""
 
 __version__ = (0, 1)
-__release__ = '.dev'
+__release__ = ''
 
 VERSION = '%d.%d' % __version__ + __release__
 
